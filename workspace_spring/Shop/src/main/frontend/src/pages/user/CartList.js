20641aa09a8f6ed1315e11fdbc3d7d2237c5058d
dlfjs4585 <dlfjs4585@gmail.com>
2024-08-19 17:50:17 +0900

    1

--- workspace_spring/Shop/src/main/frontend/src/pages/user/CartList.js
+++ workspace_spring/Shop/src/main/frontend/src/pages/user/CartList.js
@@ -9,17 +9,20 @@ const CartList = () => {
   const [cart, setCart] = useState([]);
   const{memId} = useParams();
   const[totalPrice, setTotalPrice] =useState(0);
-  const [isAllSelected, setIsAllSelected] = useState(false); // 전체 선택 상태
+  const [isAllSelected, setIsAllSelected] = useState(true); // 전체 선택 상태
 
   useEffect(() => {
     axios.get(`/api_item/getCartList/${memId}`)
     .then((res) => {
-      setCart(res.data)
-      console.log(res.data)
-      calculateTotalPrice()
+      const updatedCart = res.data.map(item => ({
+        ...item,
+        isChecked: true // 초기값을 true로 설정
+      }));
+      setCart(updatedCart)
+      calculateTotalPrice(updatedCart)
     })
     .catch((error) => {console.log(error)})
-  }, []);
+  }, [memId]);
 
   const handleQuantityChange = (index, newQuantity) => {
     // 새로운 배열을 복사해서 수정
@@ -33,9 +36,7 @@ const CartList = () => {
   // 아이템 수량 번경시 변경된 수량이 저장되는 함수
   function updateCartCnt(cartItem){
     axios.put(`/api_item/updateCartCnt`, cartItem)
-    .then((res) => {
-
-    })
+    .then((res) => {})
     .catch((error) => {
       console.log(error)
     })
@@ -46,20 +47,21 @@ const CartList = () => {
     axios.delete(`/api_item/deleteItem/${cartCode}`)
     .then((res) => {
       alert('상품이 삭제 되었습니다.')
+      window.location.reload()
     })
     .catch((error) => {console.log(error)})
   }
 
   // 체크된 품목의 총 가격을 계산
-  const calculateTotalPrice = () => {
+  const calculateTotalPrice = (updateCart = cart) => {
     let total = 0;
-    cart.forEach((cartItem, i) => {
+    updateCart.forEach((cartItem, i) => {
       if(cartItem.isChecked){
         total += cartItem.cartCnt * cartItem.itemList[0].itemPrice
       }
     });
     setTotalPrice(total)
-  }
+  };
 
   // 전체 체크박스 선택/해제 기능
   const checkedAll = (e) => {
@@ -75,12 +77,11 @@ const CartList = () => {
   const singleCheck = (index) => {
     const updateCart = [...cart]; // 기존 장바구니 배열 복사
 
-    // 선택된 아이템의 isChecked 상태 반전
     updateCart.forEach((item, i) => {
       if(i == index){
-        item.isChecked = !item.isChecked;
-      }
-    });
+          item.isChecked = !item.isChecked
+        }
+      })
     setCart(updateCart);
 
     // 전체 선택 상태 업데이트
@@ -88,56 +89,69 @@ const CartList = () => {
     setIsAllSelected(!allSelected);
   }
 
-  console.log(cart)
+  console.log(totalPrice)
 
   return (
-    <div className='cart-contain'>
-      <table className='cart-info-table'>
-        <thead>
-          <tr>
-            <td>No</td>
-            <td><input type='checkbox' checked={isAllSelected} onChange={(e) => {checkedAll(e)}} /></td>
-            <td>상품정보</td>
-            <td>가격</td>
-            <td>수량</td>
-            <td>구매가격</td>
-            <td>구매일시</td>
-            <td></td>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            cart.map((cartItem, i)=> {
-              return(
-                <tr key={i}>
-                  <td>{i}</td>
-                  <td><input type='checkbox' checked={cartItem.isChecked || false}  onChange={() => {singleCheck(i)}}/></td>
-                  <td>
-                    <div className='item-info'>
-                      <img src={`http://localhost:8080/upload/${cartItem.imgList[0].attachedFileName}`}/>
-                    {cartItem.itemList[0].itemName}
-                    </div>
-                  </td>
-                  <td>{cartItem.itemList[0].itemPrice}</td>
-                  <td>
-                    <input type='number'
-                            min={1}
-                            value={cartItem.cartCnt}
-                            onChange={(e) => {handleQuantityChange(i, parseInt(e.target.value))}}/>
-                  </td>
-                  <td>{cartItem.cartCnt*cartItem.itemList[0].itemPrice}</td>
-                  <td>{cartItem.cartDate}</td>
-                  <td>
-                    <button type='button' className='btn btn-primary' onClick={() => {deleteItem(cartItem.cartCode)}}>
-                      삭제
-                    </button>
-                  </td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
+    <div className='cart-list-div'>
+      <div className='cart-table-div'>
+        <table className='cart-list-table'>
+          <colgroup>
+              <col width='5%'/>
+              <col width='5%'/>
+              <col width='*'/>
+              <col width='10%'/>
+              <col width='10%'/>
+              <col width='12%'/>
+              <col width='20%'/>
+              <col width='10%'/>
+          </colgroup>
+          <thead>
+            <tr>
+              <td>No</td>
+              <td><input type='checkbox' checked={isAllSelected} onChange={(e) => {checkedAll(e)}} /></td>
+              <td>상품정보</td>
+              <td>가격</td>
+              <td>수량</td>
+              <td>구매가격</td>
+              <td>구매일시</td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              cart.map((cartItem, i)=> {
+                return(
+                  <tr key={i}>
+                    <td>{cart.length-i}</td>
+                    <td><input type='checkbox' checked={cartItem.isChecked || false}  onChange={() => {singleCheck(i)}}/></td>
+                    <td>
+                      <div className='img-td'>
+                        <img src={`http://localhost:8080/upload/${cartItem.imgList[0].attachedFileName}`}/>
+                      {cartItem.itemList[0].itemName}
+                      </div>
+                    </td>
+                    <td>{cartItem.itemList[0].itemPrice.toLocaleString()}</td>
+                    <td>
+                      <input type='number'
+                              className='form-control'
+                              min={1}
+                              value={cartItem.cartCnt}
+                              onChange={(e) => {handleQuantityChange(i, parseInt(e.target.value))}}/>
+                    </td>
+                    <td>￦{(cartItem.cartCnt*cartItem.itemList[0].itemPrice).toLocaleString()}</td>
+                    <td>{cartItem.cartDate}</td>
+                    <td>
+                      <button type='button' className='btn btn-primary' onClick={() => {deleteItem(cartItem.cartCode)}}>
+                        삭제
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
       <div className='total-price'>
         <p>￦ {totalPrice.toLocaleString()} 총 구매금액 </p>
       </div>
--- workspace_spring/Shop/src/main/frontend/src/pages/user/UserLayout.js
+++ workspace_spring/Shop/src/main/frontend/src/pages/user/UserLayout.js
@@ -1,13 +1,16 @@
 
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const UserLayout = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div>
       <div className='menu-div'>
         <ul className='menu-ul'>
-          <li>전체상품</li>
+          <li><span onClick={() => {navigate('/')}}>전체상품</span></li>
           <li>인터넷/IT</li>
           <li>소설/에세이</li>
           <li>자기계발</li>
